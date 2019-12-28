ea3809cbc4f2877dfc6ca61ace076af406992adb
danyyilbun <danya.boon@gmail.com>
2019-12-27 19:09:45 -0500

    Make up of the contact

--- app/src/main/java/com/example/contactmanagerexercise/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/contactmanagerexercise/adapter/RecyclerViewAdapter.java
@@ -16,6 +16,7 @@ import com.google.gson.Gson;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
@@ -27,20 +28,38 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         this.contactList = contactList;
     }
 
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_row,parent,false);
 
 
+
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
         Contact contact = contactList.get(position);
         holder.name.setText(contact.getName());
-        holder.phoneNumber.setText(contact.getPhoneNumber());
+        holder.phoneNumber.setText("+"+contact.getPhoneNumber());
+        View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+
+                Contact contact = contactList.get(position);
+                Gson gson = new Gson();
+                String json = gson.toJson(contact,Contact.class);
+
+                Intent intent = new Intent(context, SecondActivity.class  );
+                intent.putExtra("object",json);
+                context.startActivity(intent);
+            }
+        };
+             holder.crd.setOnClickListener(mOnClickListener);
+
     }
 
     @Override
@@ -48,13 +67,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return contactList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder{
         public TextView name;
         public TextView phoneNumber;
+        public CardView crd;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
+            crd= itemView.findViewById(R.id.cardView);
             name=itemView.findViewById(R.id.name);
             phoneNumber = itemView.findViewById(R.id.phone_number);
 
